35bd73d53d664fc25b01e5efd10642065210f0fb
Alex23925 <alexhernandez102598@gmail.com>
2020-11-02 21:04:55 -0800

    test scroll

--- components/Gallery.js
+++ components/Gallery.js
@@ -47,6 +47,28 @@ export default function Gallery() {
                 </div>
             </div>
 
+            <div className="gallery-wrapper-item">
+                <div className="gallery-item-content">
+                    <a href="#" className="img-link">
+                        <img variants={imgVariant} initial="initial" animate="animate" className="animated-intro-img" src="/bothBMW.jpeg" alt="Picture of the red and silver BMW" />
+                    </a>
+                    <h2 className="non-hover-title"><span className="img-title--styles">BMW - </span><span className="img-category--styles">Car</span></h2>
+                    <motion.h2 variants={h2Variant} initial="initial" animate="animate" className="hover-title--styles">Browse all photos from this session</motion.h2>
+                </div>
+            </div>
+
+            <div className="gallery-wrapper-item">
+                <div className="gallery-item-content">
+                    <a href="#" className="img-link">
+                        <img variants={imgVariant} initial="initial" animate="animate" className="animated-intro-img" src="/bothBMW.jpeg" alt="Picture of the red and silver BMW" />
+                    </a>
+                    <h2 className="non-hover-title"><span className="img-title--styles">BMW - </span><span className="img-category--styles">Car</span></h2>
+                    <motion.h2 variants={h2Variant} initial="initial" animate="animate" className="hover-title--styles">Browse all photos from this session</motion.h2>
+                </div>
+            </div>
+
+            
+
 
         </div>
     )
