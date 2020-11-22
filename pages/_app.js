aa49666adab9df2fce1e1da9feaec224937e267b
Alex23925 <alexhernandez102598@gmail.com>
2020-11-22 13:24:12 -0800

    fix lg desktop layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,7 @@
 import '../styles/globals.scss'
-import { useEffect, useState } from 'react'
-import { useRouter, Router } from 'next/router'
-import dynamic from 'next/dynamic';
-
-const Loader = dynamic(() => import("../components/Loader"));
 
 function MyApp({ Component, pageProps }) {
-
-  const [loading, setLoading] = useState(false);
-
-  Router.events.on("routerChangeStart", () => setLoading(true));
-
-  Router.events.on("routerChangeComplete", () => setLoading(false));
-
-  Router.events.on("routerChangeError", () => setLoading(false));
-
-  return loading ? <h1>Loading...</h1> : <Component {...pageProps} />
+  return <Component {...pageProps} />
 
 }
 
