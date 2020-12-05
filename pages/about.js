e1c6c2307764841249a9de66b89a5b2448756e20
Alex23925 <alexhernandez102598@gmail.com>
2020-12-04 20:51:10 -0800

    fix about page load transition

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,29 @@
 import dynamic from "next/dynamic";
 import '../styles/home.scss';
-
+import { useState, useEffect } from "react";
 const Meta = dynamic(() => import("../components/Meta"));
 const AboutHeader = dynamic(() => import("../components/AboutHeader"));
 const AboutLanding = dynamic(() => import("../components/AboutLanding"));
 const AboutBg = dynamic(() => import("../components/AboutBg"));
+const Loader = dynamic(() => import("../components/Loader"));
 
 export default function about() {
+
+    const [canScroll, setCanScroll] = useState(false);
+
+    useEffect(() => {
+        if (canScroll === false) {
+            document.querySelector("body").classList.add("no-scroll");
+        } else {
+            document.querySelector("body").classList.remove("no-scroll");
+        }
+    }, [canScroll])
+
 
     return (
         <>
             <div className={`about-page-wrapper`} >
+                <Loader setCanScroll={setCanScroll} />
                 <Meta />
                 <AboutBg />
                 <AboutHeader />
